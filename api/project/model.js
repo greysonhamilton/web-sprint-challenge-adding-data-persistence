bba060444af4f2ae4a061a9cb9fbebdab213414c
Greyson <greyson.r.hamilton@gmail.com>
2021-03-07 17:16:04 -0500

    model + router insertion

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,31 @@
 // build your `Project` model here
+const db = require("../../data/dbConfig");
+
+function getProjects() {
+
+    return db("*")
+        .from("projects")
+        .orderBy("projects_id")
+}
+
+async function addProjects(project) {
+
+    if(project.project_completed === 1) {
+        project.project_completed = true
+    } else if (project.project_completed === 0) {
+        project.project_completed = false
+    }
+
+    const [project_id] = await db("projects").insert(project)
+    return db("*")
+        .from("projects")
+        .where({project_id})
+        .first()
+
+}
+
+module.exports = {
+
+    getProjects,
+    addProjects
+};
--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,26 @@
 // build your `Resource` model here
+const db = require("../../data/dbConfig");
+
+function getResources() {
+
+    return db("*")
+        .from("resources")
+        .orderBy("resource_id")
+}
+
+async function addResource(resource) {
+
+    const [resource_id] = await db("resources")
+        .insert(resource)
+
+    return db("*")
+        .from("resources")
+        .where(resource_id)
+}
+
+module.exports = {
+
+    getResources,
+    addResource
+
+};
